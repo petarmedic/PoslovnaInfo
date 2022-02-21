7bf15520d29eea5ec1298a43c7733eab04c0f8bb
Petar Medic <mpetar97@gmailcom>
2022-02-21 21:43:29 +0100

    inicijalni komit

---
+++ PoslovnaInfo/src/main/java/com/poslovna/PoslovnaInfo/PoslovnaInfoApplication.java
@@ -0,0 +1,13 @@
+package com.poslovna.PoslovnaInfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoslovnaInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoslovnaInfoApplication.class, args);
+	}
+
+}
---
+++ PoslovnaInfo/src/test/java/com/poslovna/PoslovnaInfo/PoslovnaInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poslovna.PoslovnaInfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoslovnaInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
